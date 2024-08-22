d767c46834b2d080be84d49f938710a7c5418f04
khadythiara <diagnethiara@gmail.com>
2024-08-22 15:54:53 +0000

    premier commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from Khady!");
-        System.out.println("Hello world from DIGNE!");
+        System.out.println("Hello world from DIAGNE!");
     }
 }
