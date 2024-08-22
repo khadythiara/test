97155271d1818fa17f7d446d76a284741a57d0bd
khadythiara <diagnethiara@gmail.com>
2024-08-22 16:00:15 +0000

    premier test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from Khady!");
         System.out.println("Hello world from DIAGNE!");
+        System.out.println("Hello world from DIAGNE!");
     }
 }
