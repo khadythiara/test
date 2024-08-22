8757e211c5da65f3f5fadbe53b5c34eac4b33b94
Khadythiara <diagnethiara@gmail.com>
2024-08-22 09:34:31 -0700

    test-git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from Khady!");
         System.out.println("Hello world from DIAGNE!");
-        System.out.println("Hello world from DIAGNE!");
+        System.out.println("Hello world from je test git!");
     }
 }
