d2c44eaf665d39dcd112b3a5e0f10c05ca7f60d5
khadythiara <diagnethiara@gmail.com>
2024-08-28 15:48:51 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
         System.out.println("Hello, khady");
-=======
         System.out.println("Hello, khady!");
->>>>>>> ed3fb7d504eac0198a5ba7448160f0ab1e05646a
     }
 }
